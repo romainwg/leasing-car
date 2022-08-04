eb8475dd9e915b9b5b1b3f1aa98468d641f01324
rwalchgu <romain.walch--guinebert@capgemini.com>
2022-08-04 13:34:59 +0200

    Update type_test.go : add some test

--- pkg/api/type.go
+++ pkg/api/type.go
@@ -1,6 +1,11 @@
 package api
 
-import "time"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"time"
+)
 
 type customer struct {
 	ID                   int32     `json:"id"`
@@ -33,16 +38,101 @@ func initializeCustomer() customer {
 }
 
 func checkCustomer(c customer) bool {
-	var b bool = false
 
 	// Email regexp
-	// Name regexp
-	// Firstname regexp
-	// Birthday regexp
-	// DrivingLicenceNumber regexp
+	m, err := regexp.MatchString("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", c.Email)
+	if !m || err != nil {
+		WarningLogger.Printf("checkCustomer(%v) ; regexp.MatchString (email) ; %v\n", c, err)
+		return false
+	}
 
-	b = true
-	return b
+	// Name regexp
+	m, err = regexp.MatchString("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", c.Name)
+	if !m || err != nil {
+		WarningLogger.Printf("checkCustomer(%v) ; regexp.MatchString (name) ; %v\n", c, err)
+		return false
+	}
+
+	// Firstname regexp
+	m, err = regexp.MatchString("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", c.Firstname)
+	if !m || err != nil {
+		WarningLogger.Printf("checkCustomer(%v) ; regexp.MatchString (firstname) ; %v\n", c, err)
+		return false
+	}
+
+	// Birthday regexp
+	// Compile regexp with group
+	regexpBirthday, err := regexp.Compile("^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})Z$")
+	if err != nil {
+		WarningLogger.Printf("checkCustomer(%v) ; regexp.Compile ; %v\n", c, err)
+		return false
+	}
+
+	// Simple matching check
+	var birthdayString string = c.Birthday.Format(time.RFC3339)
+	m = regexpBirthday.MatchString(birthdayString)
+	if !m {
+		WarningLogger.Printf("checkCustomer(%v) ; regexpBirthday.MatchString(%s)\n", c, birthdayString)
+		return false
+	}
+
+	// Check all numbers
+	listDateNumberStr := regexpBirthday.FindStringSubmatch(birthdayString)
+	// Optional with previous matching
+	if len(listDateNumberStr) != 7 {
+		WarningLogger.Printf("checkCustomer(%v) ; len(listDateNumberStr) (%d) != 7\n", c, len(listDateNumberStr))
+		return false
+	}
+
+	var listDateNumberInt []int = make([]int, 0, 6)
+	for i := 1; i < 7; i++ {
+		// By pass err with previous matching
+		n, _ := strconv.Atoi(listDateNumberStr[i])
+		listDateNumberInt = append(listDateNumberInt, n)
+		fmt.Println(n)
+	}
+
+	// Simple check year - Date inf. fix ; Date sup. fix
+	if !(listDateNumberInt[0] > 1900 && listDateNumberInt[0] < 2100) {
+		WarningLogger.Printf("checkCustomer(%v) ; Simple check year (%d)\n", c, listDateNumberInt[0])
+		return false
+	}
+
+	// Simple check month - Date inf. fix ; Date sup. fix
+	if !(listDateNumberInt[1] >= 1 && listDateNumberInt[1] <= 12) {
+		WarningLogger.Printf("checkCustomer(%v) ; Simple check month (%d)\n", c, listDateNumberInt[1])
+		return false
+	}
+
+	// Simple check day - Date inf. fix ; Date sup. fix
+	if !(listDateNumberInt[2] >= 1 && listDateNumberInt[2] <= 31) {
+		WarningLogger.Printf("checkCustomer(%v) ; Simple check day (%d)\n", c, listDateNumberInt[2])
+		return false
+	}
+
+	// Get actual date
+	dateNow := time.Now()
+	dateIn := time.Date(listDateNumberInt[0], time.Month(listDateNumberInt[1]), listDateNumberInt[2], 0, 0, 0, 0, time.UTC)
+
+	diffDurationHours := int(dateNow.Sub(dateIn).Hours())
+
+	// Less than 18 years
+	if diffDurationHours < 24*356*18 {
+		WarningLogger.Printf("checkCustomer(%v) ; diffDurationHours (%d) < 24*356*18\n", c, diffDurationHours)
+		return false
+	}
+
+	// DrivingLicenceNumber regexp
+	/* m, err = regexp.MatchString("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", c.DrivingLicenceNumber)
+	if !m || err != nil {
+		return false
+	} */
+	if len(c.DrivingLicenceNumber) == 0 {
+		WarningLogger.Printf("checkCustomer(%v) ; len(c.DrivingLicenceNumber) == 0\n", c)
+		return false
+	}
+
+	return true
 }
 
 func checkCustomer2Car(c2C customer2car) bool {
--- pkg/api/type_test.go
+++ pkg/api/type_test.go
@@ -1,7 +1,203 @@
 package api
 
-import "testing"
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func init() {
+
+	log.Println("Init")
+
+	// file, err := os.OpenFile("leasing-car.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	InfoLogger = log.New(os.Stdout, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(os.Stdout, "WARNING ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stdout, "ERROR ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func TestCheckCustomerOK(t *testing.T) {
 
+	var expected bool = true
+	var ans bool = false
+
+	var inputCustomer string = `{
+		"email": "test@contact.com",
+		"name": "Nametest'",
+		"firstname": "Firstname",
+		"birthday": "1950-06-20T00:00:00Z",
+		"driving_licence_number": "TESTT654051SM9IJ"
+	}`
+	// Same condition of route with body.Reader
+	inputReader := strings.NewReader(inputCustomer)
+
+	var c customer
+
+	// Try to decode incoming json
+	err := json.NewDecoder(inputReader).Decode(&c)
+	if err != nil {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v ; %v", ans, expected, err)
+	}
+
+	ans = checkCustomer(c)
+
+	if ans != expected {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v", ans, expected)
+	}
+}
+
+func TestCheckCustomerKOEmail(t *testing.T) {
+
+	var expected bool = false
+	var ans bool = false
+
+	var inputCustomer string = `{
+		"email": "t@est@contact.com",
+		"name": "Nametest'",
+		"firstname": "Firstname",
+		"birthday": "1950-06-20T00:00:00Z",
+		"driving_licence_number": "TESTT654051SM9IJ"
+	}`
+	// Same condition of route with body.Reader
+	inputReader := strings.NewReader(inputCustomer)
+
+	var c customer
+
+	// Try to decode incoming json
+	err := json.NewDecoder(inputReader).Decode(&c)
+	if err != nil {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v ; %v", ans, expected, err)
+	}
+
+	ans = checkCustomer(c)
+
+	if ans != expected {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v", ans, expected)
+	}
+}
+
+func TestCheckCustomerKOName(t *testing.T) {
+
+	var expected bool = false
+	var ans bool = false
+
+	var inputCustomer string = `{
+		"email": "test@contact.com",
+		"name": "Nam@etest'",
+		"firstname": "Firstname",
+		"birthday": "1950-06-20T00:00:00Z",
+		"driving_licence_number": "TESTT654051SM9IJ"
+	}`
+	// Same condition of route with body.Reader
+	inputReader := strings.NewReader(inputCustomer)
+
+	var c customer
+
+	// Try to decode incoming json
+	err := json.NewDecoder(inputReader).Decode(&c)
+	if err != nil {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v ; %v", ans, expected, err)
+	}
+
+	ans = checkCustomer(c)
+
+	if ans != expected {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v", ans, expected)
+	}
+}
+
+func TestCheckCustomerKOFirstname(t *testing.T) {
+
+	var expected bool = false
+	var ans bool = false
+
+	var inputCustomer string = `{
+		"email": "test@contact.com",
+		"name": "Nametest'",
+		"firstname": "Firstnam_e",
+		"birthday": "1950-06-20T00:00:00Z",
+		"driving_licence_number": "TESTT654051SM9IJ"
+	}`
+	// Same condition of route with body.Reader
+	inputReader := strings.NewReader(inputCustomer)
+
+	var c customer
+
+	// Try to decode incoming json
+	err := json.NewDecoder(inputReader).Decode(&c)
+	if err != nil {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v ; %v", ans, expected, err)
+	}
+
+	ans = checkCustomer(c)
+
+	if ans != expected {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v", ans, expected)
+	}
+}
+
+func TestCheckCustomerKOBirthday(t *testing.T) {
+
+	var expected bool = false
+	var ans bool = false
+
+	var inputCustomer string = `{
+		"email": "test@contact.com",
+		"name": "Nametest'",
+		"firstname": "Firstname",
+		"birthday": "190-06-20T00:00:00Z",
+		"driving_licence_number": "TESTT654051SM9IJ"
+	}`
+	// Same condition of route with body.Reader
+	inputReader := strings.NewReader(inputCustomer)
+
+	var c customer
+
+	// Try to decode incoming json
+	err := json.NewDecoder(inputReader).Decode(&c)
+	if err == nil {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v ; %v", ans, expected, err)
+	}
+
+	ans = checkCustomer(c)
+
+	if ans != expected {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v", ans, expected)
+	}
+}
+
+func TestCheckCustomerKODrivingLicenceNumber(t *testing.T) {
+
+	var expected bool = false
+	var ans bool = false
+
+	var inputCustomer string = `{
+		"email": "test@contact.com",
+		"name": "Nametest'",
+		"firstname": "Firstname",
+		"birthday": "1950-06-20T00:00:00Z",
+		"driving_licence_number": ""
+	}`
+	// Same condition of route with body.Reader
+	inputReader := strings.NewReader(inputCustomer)
+
+	var c customer
+
+	// Try to decode incoming json
+	err := json.NewDecoder(inputReader).Decode(&c)
+	if err != nil {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v ; %v", ans, expected, err)
+	}
+
+	ans = checkCustomer(c)
+
+	if ans != expected {
+		t.Errorf("TestCheckCustomerOK() = %v; want %v", ans, expected)
+	}
 }
